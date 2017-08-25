3b9397d907c79e19c7fb8d05a80d26946525e5fb
chencheng <763032190@qq.com>
2017-08-25 16:50:13 +0800

    创建测试案例

--- allLesson/lesson6/main.js
+++ allLesson/lesson6/main.js
@@ -1,9 +1,18 @@
 var fibonacci = function (n) {
+    if (typeof n !== 'number') {
+        throw new Error('n should be number');
+      }
   if (n === 0) {
     return 0;
   }
   if (n === 1) {
     return 1;
+  }
+  if (n < 0) {
+    throw new Error('n should > 0');
+  }
+  if (n > 10) {
+    throw new Error('n should <= 10');
   }
   return fibonacci(n-1) + fibonacci(n-2);
 };
--- allLesson/lesson6/test/main.test.js
+++ allLesson/lesson6/test/main.test.js
@@ -8,6 +8,21 @@ describe('test/main.test.js', function () {
     it('should equal 1 when n === 1', function () {
         main.fibonacci(1).should.equal(1);
     });
+    it('should throw when n > 10', function () {
+        (function(){
+            main.fibonacci(11)
+        }).should.throw('n should <= 10');
+    });
+    it('should throw when n < 0', function () {
+        (function(){
+            main.fibonacci(-1)
+        }).should.throw('n should > 0');
+    });
+    it('should throw when n not number', function () {
+        (function(){
+            main.fibonacci('hehe')
+        }).should.throw('n should be number');
+    });
     it('should equal 55 when n === 10', function () {
         main.fibonacci(10).should.equal(55);
     });
